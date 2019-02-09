0a7de208debb72666449bb6cad7e05037d1afab7
Your Name <you@example.com>
2019-02-09 20:05:41 +0800

    'update'

--- wxfron/miniprogram/app.js
+++ wxfron/miniprogram/app.js
@@ -52,7 +52,7 @@ App({
     openid: "",
     userInfo: null,
     //默认图片
-    defaultImageUrl: 'http://image.bug2048.com/blogdefault.jpeg?',
+    defaultImageUrl: 'https://www.wuchao.shop/static/self.jpg',
     imageUrl: 'http://image.bug2048.com/',
     imageStyle200To200: 'imageView2/1/w/200/h/200/q/100',
     imageStyle600To300: 'imageView2/1/w/600/h/300/q/75|watermark/2/text/QnVn55Sf5rS7MjA0OA==/font/5a6L5L2T/fontsize/280/fill/IzAwMDAwMA==/dissolve/100/gravity/SouthEast/dx/10/dy/10'
